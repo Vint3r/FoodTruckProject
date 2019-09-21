5699fd8e9606abd7a47e58d11d1c13f7f9ccb17d
David Norris <Norris.David.Dev@gmail.com>
2019-09-21 10:40:01 -0600

    Initial push for FoodTruck project

--- src/com/skilldistillery/foodtruckproject/FoodTruck.java
+++ src/com/skilldistillery/foodtruckproject/FoodTruck.java
@@ -1,5 +1,43 @@
 package com.skilldistillery.foodtruckproject;
 
 public class FoodTruck {
+	private String truckName;
+	private String foodMade;
+	private static int truckId = 1;
+	private int truckRating;
 
+	public FoodTruck(String truckName, String foodMade, int truckRating) {
+		this.truckName = truckName;
+		this.foodMade = foodMade;
+		this.truckRating = truckRating;
+		this.truckId = truckId;
+		truckId++;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FoodTruck [truckName=");
+		builder.append(truckName);
+		builder.append(", foodMade=");
+		builder.append(foodMade);
+		builder.append(", truckRating=");
+		builder.append(truckRating);
+		builder.append(", truck Id=");
+		builder.append(truckId);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public String[] getAllTruckInfo(foodTruck[]) {
+		FoodTruck[] ftReturn = new FoodTruck[];
+		for (int i = 0; i < foodTruck.length; i++) {
+			if (foodTruck[i] == null) {
+				continue;
+			} else {
+				ftReturn[i] = foodTruck[i];
+		}
+		}
+		return ftReturn[];
+	}
 }
